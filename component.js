9eb4dff181ca60ae15a4edd5ea2b4a06e1e65a87
LocNguyen2000 <42107773+LocNguyen2000@users.noreply.github.com>
2021-03-04 16:24:27 +0700

    Add files via upload

--- component.js
+++ component.js
@@ -3,7 +3,7 @@ const component = {}
 component.header = (name, job) => {
     return `
     <div class="nav-bar">
-        <span class="logo">Portfolio</span>
+        <span class="logo">My Portfolio</span>
         <ul class="nav-contents">
             <li>Home</li>
             <li>About</li>
@@ -23,4 +23,41 @@ component.header = (name, job) => {
 
 component.body = () => {
     return `<div><div/>`
+}
+
+component.profile = () => {
+    return  `<div class="desc">
+        <div>Name: Nguyen Huu Loc</div>
+        <br>
+        <div>Gender: Male </div>
+        <br>
+        <div>Places: Hanoi, Vietnam</div>
+        <br>
+        <div>Email: locnguyenhuu2k@gmail.com </div>
+        <br>
+        <div>Phone: 0384696172 </div>
+        <br>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius impedit, ipsum ipsa nihil nulla quisquam quasi neque recusandae? </div>
+        <br>
+    </div>`
+}
+
+component.skills = () => {
+    return `
+    <div class="desc">
+        <div>HTML, CSS, Javascript </div>
+        <br>
+        <div>Python, Java, C++ </div>
+        <br>
+        <div>English </div>
+        <br>
+    </div>
+    `
+}
+
+component.tech = () => {
+    return `
+        <div class = 'desc'>
+        </div>
+    `
 }
--- main.js
+++ main.js
@@ -3,4 +3,21 @@ window.onload = init
 function init(){
     document.getElementById('header').innerHTML += component.header('Nguyen Huu Loc', 'Frontend Developer')
     document.getElementById('body').innerHTML += component.body('Loc')
+
+    aboutDesc = document.getElementById('desc-list')
+
+    // TODO: BUTTON EVENT:
+    profileBtn = document.getElementById('prf-btn')
+    profileBtn.onclick = () => {
+        aboutDesc.innerHTML = component.profile()
+    }
+
+    sklBtn = document.getElementById('skl-btn')
+    sklBtn.onclick = () => {
+        aboutDesc.innerHTML = component.skills()
+    }
+    tnlgBtn = document.getElementById('tnlg-btn')
+    tnlgBtn.onclick = () =>{
+        aboutDesc.innerHTML = component.tech()
+    }
 }
